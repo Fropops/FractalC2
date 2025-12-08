6fae33dab9db5ed66d8db7398a34209e832729c0
Olivier POTTIEZ <olivier.pottiez@live.fr>
2025-12-08 09:04:15 +0100

    Adding topolog test helpers

--- Agentv2/Program.cs
+++ Agentv2/Program.cs
@@ -55,7 +55,7 @@ namespace EntryPoint
             //connUrl = "http://192.168.48.134:2000";
             //connUrl = "pipe://127.0.0.1:Fractal";
             //
-            connUrl = "http://127.0.0.1:2000";
+            //connUrl = "http://127.0.0.1:2000";
             //connUrl = "tcp://*:4444";
 
             //connUrl = "https://127.0.0.1:3000";
@@ -155,7 +155,7 @@ namespace EntryPoint
             var hostname = Dns.GetHostName();
             var addresses = Dns.GetHostAddressesAsync(hostname).Result;
 
-            hostname += new Random().Next(0, 100);
+           
 
             var process = Process.GetCurrentProcess();
             var userName = Environment.UserName;
@@ -172,24 +172,77 @@ namespace EntryPoint
                 }
             }
 
+
+            ////////For Topological testing only
+            /*hostname += new Random().Next(0, 100);
+
+            var rand = new Random().Next(0, 100);
+            if (rand > 50)
+                integrity = IntegrityLevel.High;
+            if(rand > 75)
+                integrity = IntegrityLevel.System;
+
+            implantId =  GenerateName();*/
+
+
             AgentMetadata metadata = new AgentMetadata()
-            {
-                Id = Agent.ShortGuid.NewGuid(),
-                ImplantId = implantId,
-                Hostname = hostname,
-                UserName = userName,
-                ProcessId = process.Id,
-                Address = addresses.First(a => a.AddressFamily == AddressFamily.InterNetwork).GetAddressBytes(),
-                ProcessName = process.ProcessName,
-                Architecture = IntPtr.Size == 8 ? "x64" : "x86",
-                Integrity = integrity,
-                EndPoint = endpoint,
-                Version = "Fractal Agent .Net" + " " + Assembly.GetExecutingAssembly().GetName().Version.ToString(),
-                SleepInterval = endpoint.ToLower().StartsWith("http") ? 2 : 0, //pivoting agent
-                SleepJitter = 0
-            };
+                {
+                    Id = Agent.ShortGuid.NewGuid(),
+                    ImplantId = implantId,
+                    Hostname = hostname,
+                    UserName = userName,
+                    ProcessId = process.Id,
+                    Address = addresses.First(a => a.AddressFamily == AddressFamily.InterNetwork).GetAddressBytes(),
+                    ProcessName = process.ProcessName,
+                    Architecture = IntPtr.Size == 8 ? "x64" : "x86",
+                    Integrity = integrity,
+                    EndPoint = endpoint,
+                    Version = "Fractal Agent .Net" + " " + Assembly.GetExecutingAssembly().GetName().Version.ToString(),
+                    SleepInterval = endpoint.ToLower().StartsWith("http") ? 2 : 0, //pivoting agent
+                    SleepJitter = 0
+                };
 
             return metadata;
         }
+
+        /*public static string GenerateName()
+        {
+            var animals = new List<string>
+{
+    "Dog", "Cat", "Horse", "Cow", "Sheep", "Pig", "Chicken", "Rabbit", "Elephant", "Lion",
+    "Tiger", "Bear", "Wolf", "Fox", "Deer", "Giraffe", "Zebra", "Monkey", "Gorilla", "Kangaroo",
+    "Koala", "Leopard", "Cheetah", "Panda", "Camel", "Hippopotamus", "Rhinoceros", "Crocodile", "Alligator", "Dolphin",
+    "Whale", "Shark", "Octopus", "Seal", "Penguin", "Eagle", "Owl", "Falcon", "Parrot", "Swan",
+    "Peacock", "Goose", "Duck", "Turkey", "Rooster", "Lizard", "Snake", "Turtle", "Frog", "Toad",
+    "Crab", "Lobster", "Shrimp", "Bee", "Butterfly", "Ant", "Spider", "Fly", "Mosquito", "Dragonfly",
+    "Snail", "Worm", "Mouse", "Rat", "Squirrel", "Hedgehog", "Bat", "Raccoon", "Otter", "Beaver",
+    "Mole", "Donkey", "Mule", "Ox", "Goat", "Llama", "Reindeer", "Bison", "Buffalo", "Porcupine",
+    "Chameleon", "Iguana", "Sealion", "Stingray", "Starfish", "Seahorse", "Pigeon", "Crow", "Magpie", "Robin",
+    "Hawk", "Vulture", "Flamingo", "Pelican", "Dove", "Chimpanzee", "Baboon", "Meerkat", "Lynx", "Jaguar"
+};
+
+            var qualities = new List<string>
+{
+    "Honest", "Kind", "Brave", "Loyal", "Creative", "Patient", "Generous", "Optimistic", "Reliable", "Hardworking",
+    "Thoughtful", "Empathetic", "Caring", "Adaptable", "Confident", "Respectful", "Trustworthy", "Ambitious", "Cheerful", "Calm",
+    "Courageous", "Disciplined", "Friendly", "Helpful", "Humble", "Independent", "Inventive", "Joyful", "Modest", "Open-minded",
+    "Passionate", "Polite", "Positive", "Proactive", "Resourceful", "Sincere", "Supportive", "Tolerant", "Understanding", "Witty",
+    "Balanced", "Bright", "Charming", "Considerate", "Dependable", "Determined", "Energetic", "Fair", "Forgiving", "Gentle",
+    "Grateful", "Honorable", "Imaginative", "Innovative", "Inspiring", "Just", "Loving", "Mature", "Motivated", "Observant",
+    "Organized", "Perceptive", "Persistent", "Playful", "Practical", "Punctual", "Rational", "Realistic", "Reflective", "Reliable",
+    "Responsible", "Selfless", "Sensible", "Smart", "Sociable", "Spontaneous", "Stable", "Strong", "Sympathetic", "Talented",
+    "Trusting", "Upbeat", "Vibrant", "Warm", "Wise", "Zestful", "Clever", "Curious", "Faithful", "Focused",
+    "Honorable", "Inventive", "Open-hearted", "Patient", "Perseverant", "Respectful", "Sincere", "Tactful", "Valiant", "Visionary"
+};
+
+            var random = new Random();
+
+            string animal = animals[random.Next(animals.Count)];
+            string quality = qualities[random.Next(qualities.Count)];
+
+            string result = $"{quality}-{animal}";
+
+            return result;
+        }*/
     }
 }
