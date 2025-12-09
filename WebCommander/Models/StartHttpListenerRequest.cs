f7634463967a9072051f3cc4eae637502a284d58
Olivier POTTIEZ <olivier.pottiez@live.fr>
2025-12-09 17:43:50 +0100

    Fix listener ceration & implant creation

--- WebCommander/Models/StartHttpListenerRequest.cs
+++ WebCommander/Models/StartHttpListenerRequest.cs
@@ -1,9 +1,16 @@
 namespace WebCommander.Models
 {
+    using System.ComponentModel.DataAnnotations;
+
     public class StartHttpListenerRequest
     {
+        [Required]
         public string Name { get; set; }
+        
+        [Required]
         public string Ip { get; set; }
+        
+        [Required]
         public int BindPort { get; set; }
         public bool Secured { get; set; }
     }
